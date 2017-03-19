7b742f4639779c97e2254602cbeedda6ae23da52
Patrick Sørensen <patrick.soerensen@gmail.com>
2017-03-19 18:25:50 +0100

    Added layout to fragment
    The main activity is now just a fragment container

--- app/src/main/java/com/abdo/patrick/abdo/MainActivity.java
+++ app/src/main/java/com/abdo/patrick/abdo/MainActivity.java
@@ -2,12 +2,19 @@ package com.abdo.patrick.abdo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity {
+import layout.NewUserFragment;
+
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment fr = new NewUserFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.main_activity_fragment, fr).commit();
     }
 }
