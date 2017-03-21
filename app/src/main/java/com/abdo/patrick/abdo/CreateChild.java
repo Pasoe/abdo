f39706a561f377e97205d524110a6ff43dcec261
Patrick Sørensen <patrick.soerensen@gmail.com>
2017-03-21 12:37:04 +0100

    Added non-functional recyclerview

---
+++ app/src/main/java/com/abdo/patrick/abdo/CreateChild.java
@@ -0,0 +1,29 @@
+package com.abdo.patrick.abdo;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CreateChild extends Fragment {
+
+
+    public CreateChild() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_child, container, false);
+    }
+
+}
