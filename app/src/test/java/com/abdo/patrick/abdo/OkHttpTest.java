bcc9269ed86f5289cddceb8140f29b35dbe0e98e
Khaled <kw.k@hotmail.com>
2017-05-15 13:06:12 +0200

    Unittest

--- app/src/test/java/com/abdo/patrick/abdo/OkHttpTest.java
+++ app/src/test/java/com/abdo/patrick/abdo/OkHttpTest.java
@@ -1,8 +1,16 @@
 package com.abdo.patrick.abdo;
 
+import android.util.Log;
+
 import com.abdo.patrick.abdo.Api.OkHttp;
+import com.abdo.patrick.abdo.Domain.Application;
 import com.abdo.patrick.abdo.Models.Allergy;
+import com.abdo.patrick.abdo.Models.Anonymous;
 import com.abdo.patrick.abdo.Models.Child;
+import com.abdo.patrick.abdo.Models.Feces;
+import com.abdo.patrick.abdo.Models.Food;
+import com.abdo.patrick.abdo.Models.FoodCategory;
+import com.abdo.patrick.abdo.Models.Supplement;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -10,7 +18,15 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
@@ -21,6 +37,37 @@ import static org.junit.Assert.assertNotEquals;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class OkHttpTest {
+
+    private OkHttpClient client = new OkHttpClient();
+
+    @Test
+    public void GetControllerName() throws Exception {
+        final Request request = new Request.Builder()
+                .url("http://abdoapi.azurewebsites.net/api/Allergy")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+
+                if (!response.isSuccessful())
+                    LogFailure(response, "GET");
+                else
+                {
+                    LogSuccess(response, "GET", response.body().string());
+                    HttpUrl url = response.networkResponse().request().url();
+                    System.out.println(url.toString());
+                }
+            }
+        });
+    }
+
+
     /*@Test
     public void GetAllergies() throws Exception {
         OkHttp httpController = new OkHttp();
@@ -66,4 +113,22 @@ public class OkHttpTest {
     }*/
 
 
+    private void LogFailure(final Response response, String call){
+        Log.i("OkHttp",
+                "Failure : {"+call+"}             \n" +
+                        "Protocol: "+response.protocol()+"\n" +
+                        "Code    : "+response.code()    +"\n" +
+                        "Message : "+response.message() +"\n" +
+                        "URL     : "+response.networkResponse().request().url());
+
+    }
+
+    private void LogSuccess(final Response response, String call, String responseData){
+        Log.i("OkHttp",
+                "Success : {"+call+"}             \n" +
+                        "Code    : "+response.code()    +"\n" +
+                        "Response: "+responseData       +"\n" +
+                        "URL     : "+response.networkResponse().request().url());
+
+    }
 }
