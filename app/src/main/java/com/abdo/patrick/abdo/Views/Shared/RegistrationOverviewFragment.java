9e762aa9cd671e077d462afba109367c52b11bba
Khaled <kw.k@hotmail.com>
2017-05-18 14:14:44 +0200

    Post registrations

--- app/src/main/java/com/abdo/patrick/abdo/Views/Shared/RegistrationOverviewFragment.java
+++ app/src/main/java/com/abdo/patrick/abdo/Views/Shared/RegistrationOverviewFragment.java
@@ -4,22 +4,30 @@ package com.abdo.patrick.abdo.Views.Shared;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.abdo.patrick.abdo.Api.OkHttp;
 import com.abdo.patrick.abdo.Controllers.RegistrationOverviewController;
 import com.abdo.patrick.abdo.Domain.Application;
 import com.abdo.patrick.abdo.Models.Registration;
 import com.abdo.patrick.abdo.R;
+import com.google.gson.Gson;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RegistrationOverviewFragment extends Fragment {
 
+
+    private OkHttp okHttp;
 
     public RegistrationOverviewFragment() {
         // Required empty public constructor
@@ -34,6 +42,7 @@ public class RegistrationOverviewFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_registration_overview, container, false);
 
         model = new RegistrationOverviewController(this);
+        okHttp = new OkHttp();
 
         ArrayList<Registration> list = Application.getInstance().getCurrentChild().getRegistrations();
 
@@ -43,4 +52,26 @@ public class RegistrationOverviewFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onDetach() {
+        Log.d("onDetach", "Registration overview");
+
+        //TODO: Check if modified
+
+        ArrayList<Registration> cur_registrations = Application.getInstance().getCurrentChild().getRegistrations();
+
+        try {
+            Gson gson = new Gson();
+            String registrationApiURL = getString(R.string.api_registration);
+            String deviceId = Application.getAndroidId(getContext());
+            String JSON = gson.toJson(cur_registrations);
+            Log.d("onDetach", "Currengt registrations, JSON:\n" +JSON.toString());
+            okHttp.put(registrationApiURL+deviceId, JSON);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        super.onDetach();
+    }
 }
